2c5dce092e585a2f4dcd5bc7c5dde6833f3ac182
MOUMENE-Yassine <moumene.yassineus@gmail.com>
2023-12-07 14:03:19 -0800

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ from fabric.api import *
 import os.path
 
 env.user = 'ubuntu'
-env.hosts = ["54.146.65.131", "18.208.120.199"]
+env.hosts = ["54.157.163.64", "54.90.16.157"]
 env.key_filename = "~/.ssh/school"
 
 
