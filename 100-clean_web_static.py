d9edc1587a2b19d17c9b66c15850aaa5972e4714
MOUMENE-Yassine <moumene.yassineus@gmail.com>
2023-12-07 14:00:56 -0800

    4. Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,7 +5,7 @@ from fabric.state import commands, connections
 import os.path
 
 env.user = 'ubuntu'
-env.hosts = ["54.146.65.131", "18.208.120.199"]
+env.hosts = ["54.157.163.64", "54.90.16.157"]
 env.key_filename = "~/.ssh/school"
 
 
